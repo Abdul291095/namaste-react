70997424811c449a588f3fd55eaf882db6a73fc7
Abdul291095 <abdulrahman.softsuave@gmail.com>
2025-08-28 18:36:02 +0530

    episode - 02

--- App.js
+++ App.js
@@ -1,3 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom/client"; //-----> React 18
+// import { createRoot } from "react-dom/client"; // ----> React 19
+
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child" }, [
     React.createElement("h1", {}, "I'm h1 tag"),
@@ -11,6 +15,9 @@ const parent = React.createElement("div", { id: "parent" }, [
 
 console.log(parent);
 
+// createRoot(document.getElementById("root")).render(parent); // ----> React 19
+
+// React 18
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(parent);
